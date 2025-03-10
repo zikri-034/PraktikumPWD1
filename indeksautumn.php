692624f418418aed6d1ce9b62313659ad8daac47
Muhammad Zikri Rausyan Hermawan <124240034@student.upnyk.ac.id>
2025-03-10 18:45:09 +0700

    Add files via upload

---
+++ indeksautumn.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Look the leaf are falling!!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body style="background-color: chocolate;">
+    <div class="card mb-3 position-absolute top-50 start-50 translate-middle" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img width="450px" src="autumn.png" class="img-fluid rounded-start" alt="autumn">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Look the leaf are falling!!</h5>
+              <p class="card-text">Autumn is a season of vibrant colors and crisp air, marking the transition from summer to winter. Leaves turn shades of red, orange, and yellow, creating a breathtaking landscape. The days grow shorter, and there’s a refreshing chill in the breeze. It’s the perfect time for cozy sweaters, warm drinks, and the scent of pumpkin spice. Autumn brings a sense of change and reflection, making it one of the most cherished seasons of the year.</p>
+              <a href="quiz1_124240034.php" class="btn btn-primary" style="background-color: blueviolet;">Back to menu</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
